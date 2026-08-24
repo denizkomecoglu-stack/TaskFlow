a772fb268b76e637dd073c317693aaa6563642ea
Deniz Kömeçoğlu <denizkomecoglu@gmail.com>
2026-08-24 17:20:31 +0300

    .

--- taskflow-client/src/pages/Register.tsx
+++ taskflow-client/src/pages/Register.tsx
@@ -31,7 +31,7 @@ export default function Register() {
         setLoading(true);
 
         try {
-            await api.post('https://taskflow-vio5.onrender.com/api/Auth/register', { email, password });
+            await api.post('https://taskflow-vio5.onrender.com/api/Auth/register', {username, email, password });
             navigate('/login');
         } catch (err) {
             console.error("Kayıt hatası:", err);
