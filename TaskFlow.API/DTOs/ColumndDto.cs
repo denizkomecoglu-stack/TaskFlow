8bb9bffcef7efe0782b3f76047800e681695e2c7
Deniz Kömeçoğlu <denizkomecoglu@gmail.com>
2026-08-31 09:38:53 +0300

    tamamlandı etiketi eklendi

--- TaskFlow.API/DTOs/ColumndDto.cs
+++ TaskFlow.API/DTOs/ColumndDto.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public double Position { get; set; }
-       
+        public int Category { get; set; }
         public Guid BoardId { get; set; }
         public List<TaskItemDto> Tasks { get; set; } = new List<TaskItemDto>();
     }
