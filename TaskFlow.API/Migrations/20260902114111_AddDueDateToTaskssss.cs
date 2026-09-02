3c60e0566b2e6be900feba6fe5fdc6f131db82bb
Deniz Kömeçoğlu <denizkomecoglu@gmail.com>
2026-09-02 14:41:23 +0300

    .

---
+++ TaskFlow.API/Migrations/20260902114111_AddDueDateToTaskssss.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDueDateToTaskssss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComment_Tasks_TaskId",
+                table: "TaskComment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComment_Users_UserId",
+                table: "TaskComment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskComment",
+                table: "TaskComment");
+
+            migrationBuilder.RenameTable(
+                name: "TaskComment",
+                newName: "TaskComments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskComment_UserId",
+                table: "TaskComments",
+                newName: "IX_TaskComments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskComment_TaskId",
+                table: "TaskComments",
+                newName: "IX_TaskComments_TaskId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskComments",
+                table: "TaskComments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_Tasks_TaskId",
+                table: "TaskComments",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComments_Users_UserId",
+                table: "TaskComments",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_Tasks_TaskId",
+                table: "TaskComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskComments_Users_UserId",
+                table: "TaskComments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskComments",
+                table: "TaskComments");
+
+            migrationBuilder.RenameTable(
+                name: "TaskComments",
+                newName: "TaskComment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskComments_UserId",
+                table: "TaskComment",
+                newName: "IX_TaskComment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_TaskComments_TaskId",
+                table: "TaskComment",
+                newName: "IX_TaskComment_TaskId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskComment",
+                table: "TaskComment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComment_Tasks_TaskId",
+                table: "TaskComment",
+                column: "TaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskComment_Users_UserId",
+                table: "TaskComment",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
